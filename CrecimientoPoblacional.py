f18cf863ead3beff6aed7a1a3fe749ee72af5ad9
Alejoguzm07 <alejoguzm07@gmail.com>
2019-05-13 00:38:53 -0500

    Fractal

--- CrecimientoPoblacional.py
+++ CrecimientoPoblacional.py
@@ -1,30 +1,67 @@
+import turtle
 import urllib
 import os
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
-from sys import stdin
 import time
+from turtle import *
 
-chrome_options = Options()
-driver = None
-try:
-    driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver.exe"), chrome_options=chrome_options)
-    driver.get("https://www.worldometers.info/world-population/")
-except Exception as e:
-    driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"), chrome_options=chrome_options)
-    driver.get("https://www.worldometers.info/world-population/")
-nacimientos = set()
-for i in range(1000):
+def factor(driver):
     try:
-        valores = driver.find_elements_by_xpath('//*[@id="maincounter-wrap"]/div/span/*')
-        numero = ""
-        for j in valores:
-            numero += j.get_attribute('innerHTML')
-        numero = numero.replace(",", "")
-        nacimientos.add(int(numero))
+        valoresn = list(
+            driver.find_elements_by_xpath('/html/body/div[3]/div[2]/div[2]/div/div[3]/div[1]/div/div[2]/div[2]/span/*'))
+        valoresm = list(
+            driver.find_elements_by_xpath('/html/body/div[3]/div[2]/div[2]/div/div[3]/div[1]/div/div[3]/div[2]/span/*'))
+        factorn = valoresn[1].get_attribute('innerHTML')
+        factorm = valoresm[1].get_attribute('innerHTML')
+        return factorn, factorm
     except:
-        continue
-nacimientos = list(nacimientos)
-nacimientos.sort()
-print(nacimientos, len(nacimientos))
-driver.quit()
+        return factor(driver)
+def main():
+    chrome_options = Options()
+    driver = None
+    try:
+        driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver.exe"), chrome_options=chrome_options)
+        driver.get("https://www.worldometers.info/world-population/")
+    except Exception as e:
+        driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"), chrome_options=chrome_options)
+        driver.get("https://www.worldometers.info/world-population/")
+    nacimientos = set()
+    muertes = set()
+    t = turtle.Turtle()
+    t.color("green")
+    t2 = turtle.Turtle()
+    t2.color("red")
+    t.home()
+    t2.home()
+    factorn, factorm = factor(driver)
+
+    fn = int(factorn) / 100
+    fm = int(factorm) / 100
+    for i in range(500):
+        try:
+            time.sleep(2)
+            valoresn = list(driver.find_elements_by_xpath('/html/body/div[3]/div[2]/div[2]/div/div[3]/div[1]/div/div[2]/div[2]/span/*'))
+            valoresm = list(driver.find_elements_by_xpath('/html/body/div[3]/div[2]/div[2]/div/div[3]/div[1]/div/div[3]/div[2]/span/*'))
+            numeron = valoresn[-1].get_attribute('innerHTML')
+            numerom = valoresm[-1].get_attribute('innerHTML')
+            print(numeron, factorn)
+            print(numerom, factorm)
+            nn = int(numeron)
+            nm = int(numerom)
+            nacimientos.add(nn)
+            muertes.add(nm)
+            print(nn * fn, nm * fm)
+            t.forward(nn * fn)
+            t2.forward(nm * fm)
+            t.left(30 + (nn/100))
+            t2.left(30 + (nn/100))
+        except:
+            continue
+    nacimientos = list(nacimientos)
+    print(nacimientos, len(nacimientos))
+    muertes = list(muertes)
+    print(muertes, len(muertes))
+    driver.quit()
+main()
+
